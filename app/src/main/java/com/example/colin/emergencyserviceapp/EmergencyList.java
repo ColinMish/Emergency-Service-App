6ac7575259e01d539cc750b5d4981cc18808a566
ColinMish <colin.mackey92@gmail.com>
2015-08-19 16:26:26 +0100

    Fixed layout

--- app/src/main/java/com/example/colin/emergencyserviceapp/EmergencyList.java
+++ app/src/main/java/com/example/colin/emergencyserviceapp/EmergencyList.java
@@ -1,9 +1,11 @@
 package com.example.colin.emergencyserviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class EmergencyList extends ActionBarActivity {
@@ -34,5 +36,10 @@ public class EmergencyList extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startLocation(View view){
+        Intent intent = new Intent(this, Location.class);
+        startActivity(intent);
     }
 }
