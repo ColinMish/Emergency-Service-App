99ee94fb6eb5e8345035fb900f099eb3996b6291
Curtis Menmuir <curtismenmuir@gmail.com>
2015-08-20 23:53:57 +0100

    Code Layout
    Tidied up code

--- app/src/main/java/com/example/colin/emergencyserviceapp/ConfirmationActivity.java
+++ app/src/main/java/com/example/colin/emergencyserviceapp/ConfirmationActivity.java
@@ -1,6 +1,5 @@
 package com.example.colin.emergencyserviceapp;
 
-import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -12,34 +11,31 @@ import android.view.View;
 import android.widget.TextView;
 
 
-public class ConfirmationActivity extends ActionBarActivity {
-
+public class ConfirmationActivity extends ActionBarActivity
+{
     TextView confirmServiceView;
     TextView confirmLocationView;
     TextView confirmDescriptionView;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
         setTitle(R.string.confirmation_title);
         Log.i("SMSObject", "Confirmation activity received service: " + Integer.toString(SMSObject.getService()));
         Log.i("SMSObject", "Confirmation activity received location: " + (SMSObject.getLocation()));
         Log.i("SMSObject", "Confirmation activity received description: " + (SMSObject.getDescription()));
-
         confirmServiceView = (TextView) findViewById(R.id.confirmServiceText);
         confirmLocationView = (TextView) findViewById(R.id.confirmLocationText);
         confirmDescriptionView = (TextView) findViewById(R.id.confirmDescriptionText);
         confirmDescriptionView.setMovementMethod(new ScrollingMovementMethod());
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_confirmation, menu);
-
         int serviceEnum = SMSObject.getService();
-
         if (serviceEnum == 1)
         {
             confirmServiceView.setText("Police");
@@ -52,28 +48,24 @@ public class ConfirmationActivity extends ActionBarActivity {
         {
             confirmServiceView.setText("Fire & Rescue");
         }
-
         confirmLocationView.setText(SMSObject.getLocation());
         confirmDescriptionView.setText(SMSObject.getDescription());
-
         return true;
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
     public void onConfirmButton(View view)
     {
         try
@@ -86,9 +78,7 @@ public class ConfirmationActivity extends ActionBarActivity {
         }
         catch(Exception e)
         {
-
+            e.printStackTrace();
         }
-
     }
-
 }
--- app/src/main/java/com/example/colin/emergencyserviceapp/LocationActivity.java
+++ app/src/main/java/com/example/colin/emergencyserviceapp/LocationActivity.java
@@ -10,48 +10,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
-
 /**
  * Created by Dreads on 20/08/2015.
  */
-public class LocationActivity extends ActionBarActivity implements LocationListener {
-
-
+public class LocationActivity extends ActionBarActivity implements LocationListener
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
         setTitle(R.string.location_title);
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 300, 0, this);
     }
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_location, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
     public void refreshGPS(View view)
     {
         finish();
@@ -75,17 +69,14 @@ public class LocationActivity extends ActionBarActivity implements LocationListe
             String ad1 = add1.getText().toString();
             String ad2 = add2.getText().toString();
             String pst = post.getText().toString();
-
             String locText = (ad1 + ", " + ad2 + ", " + pst);
             SMSObject.setLocation(locText);
-
             Intent intent = new Intent(this, Situation.class);
             intent.putExtra("Exit me", true);
             startActivity(intent);
             finish();
         }
     }
-
     @Override
     public void onLocationChanged(android.location.Location location)
     {
@@ -113,19 +104,16 @@ public class LocationActivity extends ActionBarActivity implements LocationListe
             e.printStackTrace();
         }
     }
-
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
     }
-
     @Override
-    public void onProviderEnabled(String provider) {
-
+    public void onProviderEnabled(String provider)
+    {
     }
-
     @Override
-    public void onProviderDisabled(String provider) {
-
+    public void onProviderDisabled(String provider)
+    {
     }
 }
